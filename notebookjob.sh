0cd5fa030bf9ce7e4730a73c299734520d79ef85
Ebba Bergman <Ebba.Bergman@farmbio.uu.se>
2023-05-03 14:07:36 +0200

    Updated to 2023

--- notebookjob.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH -A uppmax2021-2-3 -t 0:59:00 -p core -n 15 -M snowy --gres=gpu:t4:1 --parsable --reservation=uppmax2021-2-3_06
-#CENV=${5-tf-gpu}
-#CONTAINER=${6}
-
-singularity run --nv /proj/uppmax2021-2-3/nobackup/pharmbio-notebook-tf-2.1.0.simg bash -c "jupyter notebook --ip 0.0.0.0 --port=8890 --no-browser --NotebookApp.default_url='/lab' --NotebookApp.password='' --NotebookApp.token=''"
-#singularity run --nv /proj/snic2019-8-149/nobackup/private/$CONTAINER -c "conda activate $CENV && jupyter lab --ip=127.0.0.1 --port=$2" && exit
-#singularity run --nv /proj/snic2019-8-149/nobackup/private/$CONTAINER -c "conda activate $CENV && jupyter lab --ip=127.0.0.1 --port=$3" && exit
-#singularity run --nv /proj/snic2019-8-149/nobackup/private/$CONTAINER -c "conda activate $CENV && jupyter lab --ip=127.0.0.1 --port=$4" && exit
